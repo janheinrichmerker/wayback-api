e0cf98fcf00911d98606945887a952a00207b600
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-11-21 01:04:08 +0100

    Fix test timestamp definition

--- src/test/java/dev/reimer/wayback/api/WaybackApiTest.kt
+++ src/test/java/dev/reimer/wayback/api/WaybackApiTest.kt
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.net.URL
-import java.util.*
+import java.time.LocalDateTime
 
 class WaybackApiTest {
 
     companion object {
         private const val URL_STRING = "http://example.com"
         private val URL = URL(URL_STRING)
-        private val TIMESTAMP: Calendar = Calendar.getInstance().apply {
-            set(2006, 1, 1)
-        }
+        private val TIMESTAMP: LocalDateTime =
+            LocalDateTime.of(2006, 1, 1, 0, 0, 0)
+
         private const val URL_EMPTY_STRING = "http://this.url.would.probably.never.be.indexed.by.the.web.archive"
         private val URL_EMPTY = URL(URL_EMPTY_STRING)
     }
